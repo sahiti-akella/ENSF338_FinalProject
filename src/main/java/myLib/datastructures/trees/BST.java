a3be79ecfe1a5c31621483c13aa91271c657dbc5
Sahitiakella <sahiti.akella@gmail.com>
2023-04-04 22:25:53 -0600

    Added constructors + empty functions for BST

--- src/main/java/myLib/datastructures/trees/BST.java
+++ src/main/java/myLib/datastructures/trees/BST.java
@@ -1,5 +1,77 @@
 package main.java.myLib.datastructures.trees;
 
+import main.java.myLib.datastructures.nodes.TNode;
+
 public class BST {
     
+    // Member Variables
+    private TNode root;
+
+    // Default Constructor
+    public BST() {
+        this.root = null;
+    }
+
+    // Overload Constructors
+    public BST(int val){
+        this.root.setData(val);
+    }
+
+    public BST(TNode obj){
+        this.root = obj;
+    }
+
+    // Getters and Setters
+
+    public TNode getRoot() {
+        return this.root;
+    }
+
+    public void setRoot(TNode root) {
+        this.root = root;
+    }
+
+    // Insert(int val)
+
+    public void insert(int val){
+        // Implement
+    }
+
+    // Insert(TNode node)
+
+    public void insert(TNode node){
+        // Implement
+    }
+
+    // Delete(int val)
+
+    public void delete(int val){
+        // Implement
+    }
+
+    // TNode Search(int val)
+
+    public TNode search(int val){
+         // Implement, get rid of return root
+         return root;
+    }
+
+    // printInOrder
+
+    public void printInOrder(){
+        // Implement
+    }
+
+     // printBF
+
+     public void printBF(){
+        // Implement
+    }
+
+
+
+
+
+
+
 }
