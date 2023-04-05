be8cd7092c1438ef307d16f12deb0e47f49a9c34
Sahitiakella <sahiti.akella@gmail.com>
2023-04-04 22:45:05 -0600

    Added empty functions for SLL

--- src/main/java/myLib/datastructures/linear/SLL.java
+++ src/main/java/myLib/datastructures/linear/SLL.java
@@ -1,5 +1,65 @@
 package main.java.myLib.datastructures.linear;
 
+import main.java.myLib.datastructures.nodes.SNode;
+
 public class SLL {
+
+    private SNode head;
+    private int size;
+
+    public SLL() {
+        head = null;
+        size = 0;
+    }
+
+    public SLL(SNode head) {
+        this.head = head;
+        size = 1; // CHANGE TO INCREMENT
+    }
+
+    public void insertHead(SNode node) {
+       // Implement
+    }
+
+    public void insertTail(SNode node) {
+      // Implement
+    }
+
+    public void insert(SNode node, int position){
+        // Implement
+    }
+
+    public void sortedInsert(SNode node){
+        // Implement
+    }
+
+    public void search(SNode node){
+        // Implement
+    }
+
+    public void deleteHead(){
+        // Implement
+    }
+
+    public void deleteTail(){
+        // Implement
+    }
+
+    public void delete(SNode node){
+        // Implement
+    }
+
+    public void sort(){
+        // Implement
+    }
+
+    public void clear(){
+        // Implement
+    }
+
+    public void print(){
+        // Implement
+    }
+
     
 }
