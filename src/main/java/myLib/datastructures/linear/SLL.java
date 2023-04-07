b5eb74eefe2418a49901d6b731d17c843ad6231e
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-07 01:47:25 -0600

    implemented sort, issorted, and sortedinsert for SLL class

--- src/main/java/myLib/datastructures/linear/SLL.java
+++ src/main/java/myLib/datastructures/linear/SLL.java
@@ -62,7 +62,21 @@ public class SLL {
     }
 
     public void sortedInsert(SNode node){
-        // Implement
+        if (head == null || node.getData() < head.getData()) {
+            insertHead(node);
+        } else {
+            SNode current = head;
+            while (current.getNext() != null && node.getData() > current.getNext().getData()) {
+                current = current.getNext();
+            }
+            node.setNext(current.getNext());
+            current.setNext(node);
+            size++;
+        }
+        
+        if (!isSorted()) {
+            sort();
+        }
     }
 
     public void search(SNode node){
@@ -82,7 +96,29 @@ public class SLL {
     }
 
     public void sort(){
-        // Implement
+        if (head == null || head.getNext() == null) {
+            return;
+        }
+        
+        SNode newHead = null;
+        SNode current = head;
+        while (current != null) {
+            SNode next = current.getNext();
+            if (newHead == null || current.getData() < newHead.getData()) {
+                current.setNext(newHead);
+                newHead = current;
+            } else {
+                SNode search = newHead;
+                while (search.getNext() != null && current.getData() > search.getNext().getData()) {
+                    search = search.getNext();
+                }
+                current.setNext(search.getNext());
+                search.setNext(current);
+            }
+            current = next;
+        }
+        
+        head = newHead;
     }
 
     public void clear(){
@@ -93,6 +129,18 @@ public class SLL {
         // Implement
     }
 
+
+    // helper functions
+    private boolean isSorted() {
+        SNode current = head;
+        while (current != null && current.getNext() != null) {
+            if (current.getData() > current.getNext().getData()) {
+                return false;
+            }
+            current = current.getNext();
+        }
+        return true;
+    }
 
 
     // testing functions
@@ -121,19 +169,23 @@ public class SLL {
         sList.display();  
   
         //Adding 2 to the list  
-        sList.insertTail(new SNode(2));  
-        sList.display();  
-  
-        //Adding 3 to the list  
         sList.insertTail(new SNode(3));  
         sList.display();  
   
-        //Adding 4 to the list  
+        //Adding 3 to the list  
         sList.insertTail(new SNode(4));  
+        sList.display();  
+  
+        //Adding 4 to the list  
+        sList.insertTail(new SNode(7));  
         sList.display();  
 
         sList.insert(new SNode(5), 3);  
         sList.display();
+
+        sList.sortedInsert(new SNode(6));  
+        sList.display();
+
     }  
 }  
     
