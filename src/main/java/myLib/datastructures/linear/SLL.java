7ca6d5423e43ebb3e6aad04bc4554419623af655
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-07 01:01:45 -0600

    implemented inserthead and inserttail for SLL

--- src/main/java/myLib/datastructures/linear/SLL.java
+++ src/main/java/myLib/datastructures/linear/SLL.java
@@ -5,24 +5,42 @@ import main.java.myLib.datastructures.nodes.SNode;
 public class SLL {
 
     private SNode head;
+    private SNode tail;
     private int size;
 
     public SLL() {
         head = null;
+        tail = null;
         size = 0;
     }
 
     public SLL(SNode head) {
         this.head = head;
+        tail = head;
         size = 1; // CHANGE TO INCREMENT
     }
 
     public void insertHead(SNode node) {
-       // Implement
+       if (head == null) {
+        head = node;
+        tail = node;
+        } else {
+            node.setNext(head);
+            head = node;
+        }
+        size++;
     }
 
     public void insertTail(SNode node) {
       // Implement
+      if (head == null) {
+        head = node;
+        tail = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+        }
+        size++;
     }
 
     public void insert(SNode node, int position){
@@ -61,5 +79,45 @@ public class SLL {
         // Implement
     }
 
+
+
+    // testing functions
+    public void display() {  
+        //Node current will point to head  
+        SNode current = head;  
+        if(head == null) {  
+            System.out.println("List is empty");  
+            return;  
+        }  
+        System.out.println("Adding nodes to the end of the list: ");  
+        while(current != null) {  
+            //Prints each node by incrementing pointer  
+            System.out.print(current.getData() + " ");  
+            current = current.getNext();  
+        }  
+        System.out.println();  
+    }  
+  
+    public static void main(String[] args) {  
+  
+        SLL sList = new SLL();  
+  
+        //Adding 1 to the list  
+        sList.insertHead(new SNode(1));  
+        sList.display();  
+  
+        //Adding 2 to the list  
+        sList.insertHead(new SNode(2));  
+        sList.display();  
+  
+        //Adding 3 to the list  
+        sList.insertHead(new SNode(3));  
+        sList.display();  
+  
+        //Adding 4 to the list  
+        sList.insertHead(new SNode(4));  
+        sList.display();  
+    }  
+}  
     
-}
+
--- src/main/java/myLib/datastructures/nodes/SNode.java
+++ src/main/java/myLib/datastructures/nodes/SNode.java
@@ -1,7 +1,27 @@
 package main.java.myLib.datastructures.nodes;
 
 public class SNode {
-    
+    private int data;
+    private SNode next;
 
-    // Do we need this??
+    public SNode(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public SNode getNext() {
+        return next;
+    }
+
+    public void setNext(SNode next) {
+        this.next = next;
+    }
 }
