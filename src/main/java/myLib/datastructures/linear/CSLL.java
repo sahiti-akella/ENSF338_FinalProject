302224b5b9efcd705bb56c04ee13caa788553b99
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-07 19:07:40 -0600

    override function inserthead for CSLL

--- src/main/java/myLib/datastructures/linear/CSLL.java
+++ src/main/java/myLib/datastructures/linear/CSLL.java
@@ -1,5 +1,37 @@
 package main.java.myLib.datastructures.linear;
 
+import main.java.myLib.datastructures.nodes.SNode;
 public class CSLL extends SLL {
-    
+    public CSLL() {
+        super();
+    }
+
+    public CSLL(SNode head) {
+        super(head);
+        head.setNext(head); // loop back to head node
+    }
+
+    @Override
+    public void insertHead(SNode node) {
+        if (size == 0) {
+            node.setNext(node);
+            head = node;
+            tail = node;
+        } else {
+            node.setNext(head);
+            head = node;
+            tail.setNext(head); // loop back to head node
+        }
+        size++;
+    }
+   
+    public static void main(String[] args) {  
+        CSLL csll = new CSLL();
+        csll.insertHead(new SNode(1));
+        csll.insertHead(new SNode(2));
+        csll.insertHead(new SNode(3));
+        System.out.println("Tail next node: " + csll.tail.getNext());
+        System.out.println("Tail " + csll.tail);
+
+    }  
 }
