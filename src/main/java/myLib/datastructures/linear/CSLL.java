32758588f60f3ba19f5affa1b05a6cf440f65c07
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-07 19:24:39 -0600

    override function inserttail for CSLL

--- src/main/java/myLib/datastructures/linear/CSLL.java
+++ src/main/java/myLib/datastructures/linear/CSLL.java
@@ -8,7 +8,7 @@ public class CSLL extends SLL {
 
     public CSLL(SNode head) {
         super(head);
-        head.setNext(head); // loop back to head node
+        head.setNext(head);
     }
 
     @Override
@@ -20,18 +20,33 @@ public class CSLL extends SLL {
         } else {
             node.setNext(head);
             head = node;
-            tail.setNext(head); // loop back to head node
+            tail.setNext(head);
+        }
+        size++;
+    }
+
+    @Override
+    public void insertTail(SNode node) {
+        if (size == 0) {
+            node.setNext(node);
+            head = node;
+            tail = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+            tail.setNext(head);
         }
         size++;
     }
    
+    // testing functions
     public static void main(String[] args) {  
         CSLL csll = new CSLL();
         csll.insertHead(new SNode(1));
         csll.insertHead(new SNode(2));
         csll.insertHead(new SNode(3));
+        csll.insertTail(new SNode(4));
         System.out.println("Tail next node: " + csll.tail.getNext());
         System.out.println("Tail " + csll.tail);
-
     }  
 }
