401101618e74891c46e6379887b6a83bc8cab9d9
Sahitiakella <sahiti.akella@gmail.com>
2023-04-07 18:43:05 -0600

    Initial QueueLL - need to check

--- src/main/java/myLib/datastructures/linear/QueueLL.java
+++ src/main/java/myLib/datastructures/linear/QueueLL.java
@@ -1,5 +1,49 @@
 package main.java.myLib.datastructures.linear;
 
-public class QueueLL extends SLL{
+import main.java.myLib.datastructures.nodes.SNode;
+
+public class QueueLL extends SLL {
+
+    public QueueLL() {
+        super();
+    }
     
+    public void enqueue(SNode node) {
+        super.insertTail(node);
+    }
+    
+    public void dequeue() {
+        super.deleteHead();
+    }
+
+    // Override any methods that are not applicable to queue behavior with empty body
+    @Override
+    public void sortedInsert(SNode node) {
+        // do nothing
+    }
+    
+    @Override
+    public void insertHead(SNode node) {
+        // do nothing
+    }
+    
+    @Override
+    public void insert(SNode node, int position) {
+        // do nothing
+    }
+    
+    @Override
+    public void delete(SNode node) {
+        // do nothing
+    }
+
+    @Override
+    public void deleteTail() {
+        // do nothing
+    }
+    
+    @Override
+    public void sort() {
+        // do nothing
+    }
 }
