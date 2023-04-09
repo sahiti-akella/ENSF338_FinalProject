2cf790b3077fea22a13cef1baf45b815a9661fba
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-09 14:43:55 -0600

    implemented insert

--- src/main/java/myLib/datastructures/linear/DLL.java
+++ src/main/java/myLib/datastructures/linear/DLL.java
@@ -60,6 +60,33 @@ public class DLL{
         size++;
     }
     
+    public void insert(DNode node, int position) {
+        if (position < 1 || position > size + 1) {
+            throw new IllegalArgumentException("Invalid position");
+        }
+        if (position == 1) {
+            insertHead(node);
+        } else if (position == size + 1) {
+            insertTail(node);
+        } else {
+            DNode current = head;
+            for (int i = 1; i < position; i++) {
+                current = current.getNext();
+            }
+            node.setPrev(current.getPrev());
+            node.setNext(current);
+            current.getPrev().setNext(node);
+            current.setPrev(node);
+            size++;
+            // If the list is circular, set the tail's next reference to the head
+            if (tail != null && tail.getNext() != head) {
+                tail.setNext(head);
+                head.setPrev(tail);
+            }
+        }
+    }
+    
+    
 
     public void print() {
         //System.out.println("List Length: " + size);
@@ -94,6 +121,7 @@ public class DLL{
         dList.insertTail(new DNode(3));
         dList.insertTail(new DNode(2));
         dList.insertHead(new DNode(1));  
+        dList.insert(new DNode(4), 3);
         dList.print();
     }
 }
