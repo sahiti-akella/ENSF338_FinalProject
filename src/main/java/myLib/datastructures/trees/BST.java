153db75b80ab846e2654ac6d9f82d9fa864b55f9
Sahitiakella <sahiti.akella@gmail.com>
2023-04-09 13:27:05 -0600

    added BST insert function

--- src/main/java/myLib/datastructures/trees/BST.java
+++ src/main/java/myLib/datastructures/trees/BST.java
@@ -2,76 +2,116 @@ package main.java.myLib.datastructures.trees;
 
 import main.java.myLib.datastructures.nodes.TNode;
 
+
 public class BST {
-    
-    // Member Variables
-    private TNode root;
-
-    // Default Constructor
-    public BST() {
-        this.root = null;
-    }
-
-    // Overload Constructors
-    public BST(int val){
-        this.root.setData(val);
-    }
-
-    public BST(TNode obj){
-        this.root = obj;
-    }
-
-    // Getters and Setters
-
-    public TNode getRoot() {
-        return this.root;
-    }
-
-    public void setRoot(TNode root) {
-        this.root = root;
-    }
-
-    // Insert(int val)
-
-    public void insert(int val){
-        // Implement
-    }
-
-    // Insert(TNode node)
-
-    public void insert(TNode node){
-        // Implement
-    }
-
-    // Delete(int val)
-
-    public void delete(int val){
-        // Implement
-    }
-
-    // TNode Search(int val)
-
-    public TNode search(int val){
-         // Implement, get rid of return root
-         return root;
-    }
-
-    // printInOrder
-
-    public void printInOrder(){
-        // Implement
-    }
-
-     // printBF
-
-     public void printBF(){
-        // Implement
-    }
+  
+   // Member Variables
+   private TNode root;
 
 
+   // Default Constructor
+   public BST() {
+       this.root = null;
+   }
 
 
+   // Overload Constructors
+   public BST(int val){
+       this.root = new TNode(val, 0, null, null, null);
+   }
 
+
+   public BST(TNode obj){
+       this.root = obj;
+   }
+
+
+   // Getters and Setters
+
+
+   public TNode getRoot() {
+       return this.root;
+   }
+
+
+   public void setRoot(TNode root) {
+       this.root = root;
+   }
+
+
+   // Insert(int val)
+
+
+   public void Insert(int val){
+       TNode node = new TNode(val, 0, null, null, null);
+       Insert(node);
+   }
+
+
+   // Insert(TNode node)
+
+
+   public void Insert(TNode node){
+       if (this.root == null) {
+           this.root = node;
+       }
+       else{
+           TNode current = this.root;
+       TNode parent = null;
+       while (true) {
+           parent = current;
+           if (node.getData() < current.getData()) {
+               current = current.getLeft();
+               if (current == null) {
+                   parent.setLeft(node);
+                   node.setParent(parent);
+                   return;
+               }
+           } else {
+               current = current.getRight();
+               if (current == null) {
+                   parent.setRight(node);
+                   node.setParent(parent);
+                   return;
+               }
+           }
+       }
+       }
+      
+   }
+
+
+   // Delete(int val)
+
+
+   public void Delete(int val){
+       // Implement
+   }
+
+
+   // TNode Search(int val)
+
+
+   public TNode Search(int val){
+        // Implement, get rid of return root
+        return root;
+   }
+
+
+   // printInOrder
+
+
+   public void printInOrder(TNode node){
+      // Implement
+   }
+
+
+    // printBF
+
+
+    public void printBF(){
+       // Implement
+   }
 
 
 }
