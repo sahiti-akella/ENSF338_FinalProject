0e9cf55a4cf99c7e35489c4611d75087513b978a
Sahitiakella <sahiti.akella@gmail.com>
2023-04-09 18:55:41 -0600

    Finished QueueLL + tests for it

--- src/main/java/myLib/datastructures/linear/QueueLL.java
+++ src/main/java/myLib/datastructures/linear/QueueLL.java
@@ -12,38 +12,44 @@ public class QueueLL extends SLL {
         super.insertTail(node);
     }
     
-    public void dequeue() {
+    public SNode dequeue() {
+        if (head == null) {
+            return null;
+        }
+        SNode node = head;
         super.deleteHead();
+        return node;
+    }
+
+    public SNode peek() {
+        if (head == null) {
+            return null;
+        }
+        return head;
     }
 
     // Override any methods that are not applicable to queue behavior with empty body
     @Override
     public void sortedInsert(SNode node) {
-        // do nothing
     }
     
     @Override
     public void insertHead(SNode node) {
-        // do nothing
     }
     
     @Override
     public void insert(SNode node, int position) {
-        // do nothing
     }
     
     @Override
     public void delete(SNode node) {
-        // do nothing
     }
 
     @Override
     public void deleteTail() {
-        // do nothing
     }
     
     @Override
     public void sort() {
-        // do nothing
     }
 }
--- src/main/java/myLib/test/linearTests/QueueLLTests.java
+++ src/main/java/myLib/test/linearTests/QueueLLTests.java
@@ -4,5 +4,33 @@ import main.java.myLib.datastructures.nodes.*;
 import main.java.myLib.datastructures.linear.*;
 
 public class QueueLLTests {
+    public static void main(String[] args) {
+        
+        // Create a new queue
+        QueueLL queue = new QueueLL();
+        
+        // Enqueue some nodes
+        SNode node1 = new SNode(1);
+        SNode node2 = new SNode(2);
+        SNode node3 = new SNode(3);
+        queue.enqueue(node1);
+        queue.enqueue(node2);
+        queue.enqueue(node3);
+        
+        // Test peek method
+        System.out.println("Peek: " + queue.peek().getData());
+        
+        // Dequeue nodes and print them
+        System.out.println("Dequeue: " + queue.dequeue().getData());
+        System.out.println("Dequeue: " + queue.dequeue().getData());
+        System.out.println("Dequeue: " + queue.dequeue().getData());
+        
+        // Test dequeue on empty queue
+        System.out.println("Dequeue: " + queue.dequeue());
+        
+        // Test enqueue after dequeuing all nodes
+        queue.enqueue(node1);
+        System.out.println("Peek: " + queue.peek().getData());
+    }
     
 }
