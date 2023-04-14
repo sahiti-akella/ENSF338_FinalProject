b1f9cf2cc10c9711dda50ec9fdb9c048f7ee75eb
Sahitiakella <sahiti.akella@gmail.com>
2023-04-14 07:38:06 -0600

    finished stackLL + tests

--- src/main/java/myLib/datastructures/linear/StackLL.java
+++ src/main/java/myLib/datastructures/linear/StackLL.java
@@ -4,72 +4,127 @@ import main.java.myLib.datastructures.nodes.SNode;
 
 public class StackLL extends SLL {
 
-    // Default Constructor
+    /**
+    * This is a Stack based on Singly Linked List data structure and extends the singlyLL
+    */
+
+    /**
+    * Empty Stack
+    */
     public StackLL() {
         super();
     }
 
+    /**
+    * Empty Stack consisting of argument node
+    * @param node the SNode to add to the stack
+    */
+    public StackLL(SNode<Integer> node) {
+        super(node);
+    }
+
+    /**
+     * Adds argument node to the start of the stack
+     * @param node the SNode to add to the stack
+     */
     public void push(SNode<Integer> node) {
         super.insertHead(node);
     }
 
+     /**
+     * Deletes the head of the stack if not null
+     * @return Returns the popped element
+     */
     public SNode<Integer> pop() {
         SNode<Integer> node = head;
         if (head != null) {
-            deleteHead();
+            super.deleteHead();
         }
         return node;
     }
 
+     /**
+     * Returns the head of the stack
+     * @return Returns the node at head of stack
+     * 
+     */
     public SNode<Integer> peek() {
-        return head;
+        return this.head;
     }
 
+     /**
+     * No functionality in StackLL
+     * @param node the SNode to add to the list
+     */
     @Override
-    public void insertHead(SNode<Integer> node) {
-        super.insertHead(node);
-    }
+    public void insertHead(SNode<Integer> node) {}
 
+     /**
+     * No functionality in StackLL
+     * @param node the SNode to add to the list
+     */
     @Override
-    public void insertTail(SNode<Integer> node) {
-        // Override with empty body to avoid misuse
-    }
+    public void insertTail(SNode<Integer> node) {}
 
+     /**
+     * No functionality in StackLL
+     * @param node the SNode to add to the stack
+     * @param position the SNode to add to the stack
+     */
     @Override
-    public void insert(SNode<Integer> node, int position) {
-        super.insert(node, position);
-    }
+    public void insert(SNode<Integer> node, int position) {}
 
+     /**
+     * No functionality in StackLL
+     * @param node the SNode to add to the list
+     */
     @Override
-    public void sortedInsert(SNode<Integer> node) {
-        super.sortedInsert(node);
-    }
+    public void sortedInsert(SNode<Integer> node) {}
 
+     /**
+     * Searches for given node in stack
+     * @param node Returns node if found, else returns null
+     */
     @Override
     public SNode<Integer> search(SNode<Integer> node) {
-        return super.search(node);
+        SNode<Integer> currentNode = this.head;
+        while (currentNode != null) {
+            if (currentNode == node) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return null;
     }
 
+    /**
+     * No functionality in StackLL
+     */
     @Override
-    public void deleteHead() {
-        super.deleteHead();
-    }
+    public void deleteHead() {}
 
+    /**
+     * No functionality in StackLL
+     */
     @Override
-    public void deleteTail() {
-        // Override with empty body to avoid misuse
-    }
+    public void deleteTail() {}
 
+     /**
+     * No functionality in StackLL
+     * @param node the SNode to add to the list
+     */
     @Override
-    public void delete(SNode<Integer> node) {
-        super.delete(node);
-    }
+    public void delete(SNode<Integer> node) {}
 
+    /**
+     * No functionality in StackLL
+     */
     @Override
-    public void sort() {
-        super.sort();
-    }
+    public void sort() {}
 
+    /**
+     * Clears the stack
+     */
     @Override
     public void clear() {
         super.clear();
@@ -77,7 +132,13 @@ public class StackLL extends SLL {
 
     @Override
     public void print() {
-        super.print();
+        SNode<Integer> current = head;
+        System.out.print("Stack: ");
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
     }
 }
 
--- src/main/java/myLib/test/linearTests/StackLLTests.java
+++ src/main/java/myLib/test/linearTests/StackLLTests.java
@@ -4,5 +4,49 @@ import main.java.myLib.datastructures.nodes.*;
 import main.java.myLib.datastructures.linear.*;
 
 public class StackLLTests {
+
+    public static void main(String[] args) {
+        StackLL stack = new StackLL();
+        
+        System.out.println("Testing empty constructor...");
+        stack.print(); 
+        
+        System.out.println("\nTesting constructor with argument node...");
+        SNode<Integer> node1 = new SNode<Integer>(1);
+        stack = new StackLL(node1);
+        stack.print(); 
+        
+        // Test push method
+        System.out.println("\nTesting push method...");
+        SNode<Integer> node2 = new SNode<Integer>(2);
+        SNode<Integer> node3 = new SNode<Integer>(3);
+        stack.push(node2);
+        stack.push(node3);
+        stack.print(); 
+        
+        // Test pop method
+        System.out.println("\nTesting pop method...");
+        SNode<Integer> popped = stack.pop();
+        System.out.println("Popped node: " + popped.getData()); // expected output: Popped node: 3
+        stack.print(); // expected output: Stack: 2 1
+        
+        // Test peek method
+        System.out.println("\nTesting peek method...");
+        SNode<Integer> peeked = stack.peek();
+        System.out.println("Peeked node: " + peeked.getData()); // expected output: Peeked node: 2
+        
+        // Test search method
+        System.out.println("\nTesting search method...");
+        SNode<Integer> node4 = new SNode<Integer>(4);
+        SNode<Integer> found = stack.search(node1);
+        System.out.println("Found node: " + found.getData()); // expected output: Found node: 1
+        found = stack.search(node4);
+        System.out.println("Found node: " + found); // expected output: Found node: null
+        
+        // Test clear method
+        System.out.println("\nTesting clear method...");
+        stack.clear();
+        stack.print(); // expected output: Stack:
+    }
     
 }
